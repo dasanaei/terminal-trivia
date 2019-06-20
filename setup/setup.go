85ab0636c36a8b110d20ae960fe908e6f3028edc
Dante <dsanaei@purdue.edu>
2019-06-20 04:34:19 -0400

    Setup Process

--- setup/setup.go
+++ setup/setup.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	winSetup()
-	fmt.Printf("TEST")
 }
 
 func winSetup() {
-	dir, _ := os.UserHomeDir()
-	dir = dir + "\\astral-kuarry"
-	fmt.Println(dir)
-	if CreateDirIfNotExist(dir) {
-		fmt.Print("sucess")
+	homeDir, _ := os.UserHomeDir()
+	triviaDir := homeDir + "\\astral-kuarry\\trivia"
+	dataDir := triviaDir + "\\data"
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
 	}
+	exPath := filepath.Dir(ex)
+	dist := strings.Replace(exPath, "setup", "dist", -1)
+	data := strings.Replace(exPath, "setup", "data", -1)
+	CreateDirIfNotExist(triviaDir)
+	CreateDirIfNotExist(dataDir)
+	CopyDir(dist, triviaDir)
+	CopyDir(data, dataDir)
+	oldLocation := triviaDir + "\\main.exe"
+	newLocation := homeDir + "\\trivia.exe"
+	os.Rename(oldLocation, newLocation)
+
+}
+
+func getCommandOutput(cmd *exec.Cmd) string {
+	out, err := cmd.CombinedOutput()
+	result := string(out)
+	if err != nil {
+		result += err.Error()
+	}
+	return result
 }
 
 //CreateDirIfNotExist does exactly what it says
@@ -29,4 +53,98 @@ func CreateDirIfNotExist(dir string) bool {
 		return true
 	}
 	return false
+}
+
+func CopyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	defer sourcefile.Close()
+
+	destfile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+		}
+
+	}
+
+	return
+}
+
+func CopyDir(source string, dest string) (err error) {
+
+	// get properties of source dir
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	// create dest dir
+
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+
+	directory, _ := os.Open(source)
+
+	objects, err := directory.Readdir(-1)
+
+	for _, obj := range objects {
+
+		sourcefilepointer := source + "/" + obj.Name()
+
+		destinationfilepointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			// create sub-directories - recursively
+			err = CopyDir(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			// perform copy
+			CopyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	}
+	return
+}
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
